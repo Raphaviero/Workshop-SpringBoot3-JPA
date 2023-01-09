5af9ff0e3ade928a8967c5f089d4b7bba9ccd62b
Raphael Viero <raphaviero@gmail.com>
2023-01-09 20:13:14 -0300

    H2 database, test profile and JPA

--- src/main/java/com/projetoPessoal/course/resources/UserResource.java
+++ src/main/java/com/projetoPessoal/course/resources/UserResource.java
@@ -12,8 +12,8 @@ import com.projetoPessoal.course.entities.User;
 public class UserResource {
 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "Maria@gmail.com", "999999999", "12345");
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
