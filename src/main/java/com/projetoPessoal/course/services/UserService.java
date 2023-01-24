94993250061f877e4459cc365f1223eddf000450
Raphael Viero <raphaviero@gmail.com>
2023-01-24 20:31:59 -0300

    Exception handling - update

--- src/main/java/com/projetoPessoal/course/services/UserService.java
+++ src/main/java/com/projetoPessoal/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetoPessoal.course.repositories.UserRepository;
 import com.projetoPessoal.course.services.exceptions.DataBaseException;
 import com.projetoPessoal.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,9 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 
